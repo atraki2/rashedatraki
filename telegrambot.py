59be4d3b2a171201f9eb9078fb5a9026a105ec8c
atraki2 <rashedytiran@gmail.com>
2025-07-14 20:22:20 +0300

    Delete telegrambot.py

--- telegrambot.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot('7790340259:AAGfKIrZ8EC0tcW5ZBrtXuC9kY8B3tdm2H0')
-
-@bot.message_handler(['start'])
-def send_start(message):
-  bot.reply_to(message, 'hi man')
-  bot.polling()
-
